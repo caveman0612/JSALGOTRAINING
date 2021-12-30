fdb5b76f61f5ddff38b97f41941d6e158fea424d
cavem <kyle.stinar@hotmail.com>
2021-12-29 18:55:15 -0800

    starting files

---
+++ main/linkedListConstruction/linkedListConstruction.js
@@ -0,0 +1,64 @@
+// This is an input class. Do not edit.
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+// Feel free to add new properties and methods to the class.
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  setHead(node) {
+    // Write your code here.
+  }
+
+  setTail(node) {
+    // Write your code here.
+  }
+
+  insertBefore(node, nodeToInsert) {
+    // Write your code here.
+  }
+
+  insertAfter(node, nodeToInsert) {
+    // Write your code here.
+  }
+
+  insertAtPosition(position, nodeToInsert) {
+    // Write your code here.
+  }
+
+  removeNodesWithValue(value) {
+    // Write your code here.
+  }
+
+  remove(node) {
+    if (node === this.head) this.head = this.head.next;
+    if (node === this.tail) this.tail = this.tail.prev;
+    this.detachNodeFromList(node);
+  }
+
+  containsNodeWithValue(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value == value) return true;
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
+
+  detachNodeFromList(node) {
+    if (node.prev !== null) node.prev.next = node.next;
+    if (node.next !== null) node.next.prev = node.prev;
+    node.prev = null;
+    node.next = null;
+  }
+}
+
+module.exports = { Node, DoublyLinkedList };
---
+++ main/linkedListConstruction/main.js
@@ -0,0 +1,19 @@
+const { Node, DoublyLinkedList } = require("./linkedListConstruction");
+// import { Node, DoublyLinkedList } from "./linkedListConstruction";
+
+const one = new Node(1);
+const two = new Node(2);
+const three = new Node(3);
+one.next = two;
+
+two.next = three;
+two.prev = one;
+
+three.prev = two;
+const linkedList = new DoublyLinkedList();
+linkedList.head = one;
+linkedList.tail = three;
+
+linkedList.remove(two);
+
+console.log(one, three);
